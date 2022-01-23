c1e86fc2e60c07fc8fe3df84eb4bca9586ba58f2
muhammad-khair <muhammad_khair@outlook.com>
2022-01-23 22:34:43 +0800

    Add template spring framework

---
+++ src/main/java/com/mjkrt/rendr/RendrApplication.java
@@ -0,0 +1,13 @@
+package com.mjkrt.rendr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RendrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RendrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mjkrt/rendr/RendrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mjkrt.rendr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RendrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
