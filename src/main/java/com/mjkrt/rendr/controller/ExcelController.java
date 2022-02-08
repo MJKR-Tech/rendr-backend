da80f853294545ebb22586196f44835ef002ab18
eezj35 <justineezj0@gmail.com>
2022-02-08 18:50:14 +0800

    Implement functionality of reading in nested simple json file
    Problems:
    *ISIN field seem to be causing problems; with isin field present there is an error when reading the json,
    after removing isin field from all rows it works

--- src/main/java/com/mjkrt/rendr/controller/ExcelController.java
+++ src/main/java/com/mjkrt/rendr/controller/ExcelController.java
@@ -1,18 +1,36 @@
 package com.mjkrt.rendr.controller;
 
 import java.io.ByteArrayInputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.net.URL;
 import java.util.List;
 import java.util.logging.Logger;
 
 import javax.servlet.http.HttpServletResponse;
 
+import com.fasterxml.jackson.core.Base64Variant;
+import com.fasterxml.jackson.core.JsonLocation;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonStreamContext;
+import com.fasterxml.jackson.core.JsonToken;
+import com.fasterxml.jackson.core.ObjectCodec;
+import com.fasterxml.jackson.core.Version;
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectReader;
 import com.mjkrt.rendr.entity.SimpleRow;
+import net.bytebuddy.build.Plugin;
 import org.apache.commons.compress.utils.IOUtils;
+import org.apache.tomcat.util.json.JSONParser;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -40,9 +58,16 @@ public class ExcelController {
         response.setHeader("Content-Disposition", "attachment; filename=sampleData.xlsx");
 
         ObjectMapper mapper = new ObjectMapper();
-        TypeReference<List<SimpleRow>> typeReference = new TypeReference<>(){};
-        InputStream inputStream = TypeReference.class.getResourceAsStream("/json/Simple.json");
-        List<SimpleRow> simpleRows = mapper.readValue(inputStream, typeReference);
+        ObjectReader reader = mapper.readerFor(new TypeReference<List<SimpleRow>>() {});
+//        InputStream inputStream = TypeReference.class.getResourceAsStream();
+
+        File file = new ClassPathResource("json/Simple.json").getFile();
+        JsonNode data = mapper.readTree(file);
+        JsonNode body = data.path("body");
+        JsonNode report = body.path("SIMPLE_REPORT");
+        JsonNode rows = report.path("rows");
+        List<SimpleRow> simpleRows = reader.readValue(rows);
+
 
         ByteArrayInputStream stream = service.generateWorkBook(simpleRows);
         IOUtils.copy(stream, response.getOutputStream());
