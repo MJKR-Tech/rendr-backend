7a73c959feba1e123dac90454dcc04206c0a03cb
Michael Lee Sheng Kiat <michaeleelele@gmail.com>
2022-04-06 22:58:04 +0800

    Update tableholder to include comparator

--- src/main/java/com/mjkrt/rendr/entity/helper/TableHolder.java
+++ src/main/java/com/mjkrt/rendr/entity/helper/TableHolder.java
@@ -1,14 +1,23 @@
 package com.mjkrt.rendr.entity.helper;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
+import java.util.function.BiFunction;
+import java.util.stream.Collectors;
 
 public class TableHolder {
-    
+
+    private static final BiFunction<Integer, Boolean, Comparator<List<String>>> comparatorGenerator = (idx, isAsc)
+            -> (list1, list2) -> list1.get(idx).compareTo(list2.get(idx)) * ((isAsc) ? 1 : -1);
+
     private List<ColumnHeader> columnHeaders;
-    
+
+    private Optional<Comparator<List<String>>> optionalComparator = Optional.empty();
+
     private Set<List<String>> dataRows = new HashSet<>();
 
     public TableHolder(List<ColumnHeader> columnHeaders, Set<List<String>> dataRows) {
@@ -21,9 +30,11 @@ public class TableHolder {
         this.columnHeaders = columnHeaders;
     }
 
-
     private static void verifyStructure(List<ColumnHeader> columnHeaders, Set<List<String>> dataRows) {
         int headerSize = columnHeaders.size();
+        if (headerSize == 0) {
+            throw new IllegalArgumentException("Headers cannot be empty");
+        }
         for (List<String> row : dataRows) {
             if (row.size() == headerSize) {
                 continue;
@@ -43,7 +54,7 @@ public class TableHolder {
     public Set<List<String>> getDataRows() {
         return dataRows;
     }
-    
+
     public void setDataRows(Set<List<String>> dataRows) {
         this.dataRows = dataRows;
     }
@@ -51,7 +62,22 @@ public class TableHolder {
     public void setDataRow(List<String> dataRow) {
         this.dataRows.add(dataRow);
     }
-    
+
+    public void setSortColumnAndDirection(ColumnHeader header, boolean isAscending) {
+        int idx = columnHeaders.indexOf(header);
+        if (idx < 0) {
+            return; // not found
+        }
+        optionalComparator = Optional.of(comparatorGenerator.apply(idx, isAscending));
+    }
+
+    public List<List<String>> generateOrderedTable() {
+        Comparator<List<String>> comparator = optionalComparator.orElse(Comparator.comparing(list -> list.get(0)));
+        return dataRows.stream()
+                .sorted(comparator)
+                .collect(Collectors.toList());
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) {
@@ -62,18 +88,20 @@ public class TableHolder {
         }
         TableHolder that = (TableHolder) o;
         return Objects.equals(columnHeaders, that.columnHeaders)
+                && Objects.equals(optionalComparator, that.optionalComparator)
                 && Objects.equals(dataRows, that.dataRows);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(columnHeaders, dataRows);
+        return Objects.hash(columnHeaders, optionalComparator, dataRows);
     }
 
     @Override
     public String toString() {
         return "TableHolder{" +
                 "columnHeaders=" + columnHeaders +
+                ", optionalComparator=" + optionalComparator +
                 ", dataRows=" + dataRows +
                 '}';
     }
